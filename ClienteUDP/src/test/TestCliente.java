8a3397f8d790815ffccf51ab94914b76864ac1bd
José Vargas <tu-correo@ejemplo.com>
2026-04-18 15:36:43 -0500

    Cliente y Servidor UDP

---
+++ ClienteUDP/src/test/TestCliente.java
@@ -0,0 +1,8 @@
+
+package test;
+
+public class TestCliente {
+    public static void main(String[] args) throws Exception {
+        // para pruebas por consola
+    }
+}
---
+++ ServidorUDP/src/test/TestServidor.java
@@ -0,0 +1,11 @@
+package test;
+
+import entidades.Servidor;
+
+public class TestServidor {
+    public static void main(String[] args) throws Exception {
+
+        (new Servidor()).operar(5000);
+
+    }
+}
